476f0bb0a900dc7015f6ee072d47e55c11fa2c8e
Piyas <piyasreza31@gmail.com>
2025-08-09 02:43:52 +0600

    solve all version problem

--- app.py
+++ app.py
@@ -87,5 +87,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run()
+
