12b78c21bd3a0903ac0899d34988a0776b6f594d
Piyas <piyasreza31@gmail.com>
2025-08-09 02:50:10 +0600

    again fixed issue

--- app.py
+++ app.py
@@ -87,6 +87,5 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
-if __name__ == "__main__":
-    app.run()
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
